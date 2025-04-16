2f998c1d88165b597cad98888c048bf7e31c7025
Youssef <uosef2111@gmail.com>
2025-04-16 14:03:23 +0200

    feat: Set up routing and update NavBar links with routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductsComponent } from './products/products.component';
+import { SettingsComponent } from './settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: SettingsComponent},
+    {path: 'about', component:AboutComponent},
+    {path: 'categories', component:CategoriesComponent},
+    {path: 'products', component:ProductsComponent}
+];
--- src/app/navBar/navbar.component.ts
+++ src/app/navBar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, input } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: "app-navBar",
+    imports: [RouterLink],
     templateUrl: "./navbar.component.html",
     styleUrls: ["./navbar.component.css"],
 
